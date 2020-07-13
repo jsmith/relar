f0eb53c1f889162c7a9856bb759ffc0bca20b109
Jacob Smith <jacob.smith@unb.ca>
2020-07-12 21:08:49 -0300

    adding support for staging

--- packages/scripts/src/provision-account.ts
+++ packages/scripts/src/provision-account.ts
@@ -1,32 +1,45 @@
 import * as admin from "firebase-admin";
 import * as uuid from "uuid";
-// TODO put this somewheres (maybe 1password?)
 import * as serviceAccount from "../../serviceAccountKey.toga-4e3f5.json";
+import * as serviceAccountStaging from "../../serviceAccountKey.relar-staging.json";
 import { BetaSignup } from "./shared/types";
 import * as sgMail from "@sendgrid/mail";
 import { env } from "./env";
 
 sgMail.setApiKey(env.mail.sendgrid_api_key);
 
-admin.initializeApp({
-  databaseURL: "https://toga-4e3f5.firebaseio.com",
-  storageBucket: "toga-4e3f5.appspot.com",
-  projectId: "toga-4e3f5",
-  credential: admin.credential.cert(serviceAccount as any),
-});
+const argv = process.argv.slice(0);
+let staging = false;
+if (argv[2] === "--staging") {
+  argv.splice(2, 1);
+  staging = true;
+
+  admin.initializeApp({
+    databaseURL: "https://relar-staging.firebaseio.com",
+    storageBucket: "relar-staging.appspot.com",
+    projectId: "relar-staging",
+    credential: admin.credential.cert(serviceAccountStaging as any),
+  });
+} else {
+  admin.initializeApp({
+    databaseURL: "https://toga-4e3f5.firebaseio.com",
+    storageBucket: "toga-4e3f5.appspot.com",
+    projectId: "toga-4e3f5",
+    credential: admin.credential.cert(serviceAccount as any),
+  });
+}
 
 const auth = admin.auth();
 const firestore = admin.firestore();
 
 const main = async () => {
-  if (process.argv.length !== 3) {
+  if (argv.length !== 3) {
     console.error("Please provide a single argument.");
     return;
   }
 
   const token = uuid.v4();
-
-  const email = process.argv[2];
+  const email = argv[2];
   const ref = firestore.doc(`beta_signups/${email}`);
   const doc = await ref.get();
   if (!doc.exists) {
