13e853b47f3db1d7908cd2226021b15530655f8a
Jacob Smith <jacob.smith@unb.ca>
2020-08-10 07:45:20 -0300

    init events and watcher tests

--- packages/app/src/watcher.test.ts
+++ packages/app/src/watcher.test.ts
@@ -4,10 +4,38 @@ import "./load-env";
 import { useFirebaseUpdater } from "./watcher";
 import { renderHook, act } from "@testing-library/react-hooks";
 
-test("watcher can test", () => {
-  const snap = ({ path: "test/ref" } as unknown) as firebase.firestore.QueryDocumentSnapshot<{}>;
-  // useFirebaseUpdater(snap);
-  assert.ok(true);
+const create = (name: string) =>
+  ({
+    ref: { path: "test/ref" },
+    data: () => ({ name }),
+  } as firebase.firestore.QueryDocumentSnapshot<{ name: string }>);
+
+test("watcher returns a default value", () => {
+  const snap = create("a");
+  const { result } = renderHook(() => useFirebaseUpdater(snap));
+  assert.equal(result.current[0].name, "a");
+});
+
+test("returns the cached value", () => {
+  const a = create("a");
+  const b = create("b");
+  renderHook(() => useFirebaseUpdater(a));
+  const { result } = renderHook(() => useFirebaseUpdater(b));
+  assert.equal(result.current[0].name, "a");
+});
+
+test("can update values", () => {
+  const a = create("a");
+  const b = create("b");
+  const hookA = renderHook(() => useFirebaseUpdater(a));
+  const hookB = renderHook(() => useFirebaseUpdater(b));
+  hookA.result.current[1]({ name: "c" });
+  assert.equal(hookA.result.current[0].name, "c");
+  assert.equal(hookB.result.current[0].name, "c");
+  hookB.unmount();
+  hookA.result.current[1]({ name: "d" });
+  assert.equal(hookA.result.current[0].name, "d");
+  assert.equal(hookB.result.current[0].name, "c");
 });
 
 test.run();
