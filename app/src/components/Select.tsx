3748b13c7424fcdac050355036d8305f9eaaf0b0
Jacob Smith <jsmith@hey.com>
2020-10-04 10:12:26 -0300

    Beta Signup Additional Info (#73)
    * added name, device to beta signup
    * Moved deps back to `app/` except for shared deps
    * Fixed height issues where pages did not stretch to fit the screen

---
+++ app/src/components/Select.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import classNames from "classnames";
+
+export interface SelectOption<T extends string> {
+  value: T;
+  label: string;
+}
+
+export interface SelectProps<T extends string> {
+  className?: string;
+  options: Array<SelectOption<T>>;
+  selected: T | undefined;
+  onSelect: (value: T) => void;
+  label?: string;
+}
+
+export const Select = function <T extends string>({
+  className,
+  options,
+  selected,
+  onSelect,
+  label,
+}: SelectProps<T>) {
+  return (
+    <label className={classNames("block")}>
+      {label && <span className={classNames("mb-1")}>{label}</span>}
+      <select
+        value={selected}
+        onChange={(v) => {
+          onSelect(v.target.value as T);
+        }}
+        className={classNames("form-select mt-1 block w-full", className)}
+      >
+        {options.map((o) => (
+          <option key={o.value} value={o.value}>
+            {o.label}
+          </option>
+        ))}
+      </select>
+    </label>
+  );
+};
